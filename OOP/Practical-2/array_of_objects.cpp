d76bd1c354711010c7784d4cbde1dc7690851661
The_Valour <108991640+TheValour@users.noreply.github.com>
2022-11-29 12:02:22 +0530

    we can now entered the no. of student

--- OOP/Practical-2/array_of_objects.cpp
+++ OOP/Practical-2/array_of_objects.cpp
@@ -1,36 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 class student {
     string name;
     public:
-        void getdata();
-        void display();
-        void sortString();
- };
- void student::getdata() {
-    cout<<"Enter the name: ";
-    getline(cin,name);
- }
- void student::display() {
-     cout << name << endl;
- }
- void student::sortString() {
-     sort(name.begin(), name.end());
- }
-
+        void getdata(){
+            cout<<"Enter the name: ";
+            cin>>name;
+        }
+        void display(){
+            cout << name << endl;
+        }
+        void sortString(){
+            sort(name.begin(), name.end());
+        }
+};
  int main() {
-    student s[2];	
+    int n;
+    cout<<"Enter no. of student : ";
+    cin>>n;
+    student s[n];	
     cout<<"Note: name should be in lowercase."<<endl;
-    for(int i=0;i<2;i++) {
+    
+    for(int i=0;i<n;i++) {
         s[i].getdata();
     }
     cout << "After String is Sorted." << endl;
-    for(int i=0;i<2;i++) {
+    for(int i=0;i<n;i++) {
         s[i].sortString();
     }
-    for(int j=0;j<2;j++) {
-        cout << "Name 1: ";
+    for(int j=0;j<n;j++) {
+        cout <<"Name "<<j+1<<" :";
         s[j].display();
     }
     return 0;
